9866516ac82c4f5e0badd590044efeb239e21c54
sabbir-rupom <sabbir.rupom@gmail.com>
2019-03-08 11:38:56 +0600

    API description added

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
+/*
+ * RESTful API Template
+ * 
+ * A RESTful API template based on flight-PHP framework
+ * This software project is based on my recent REST-API development experiences. 
+ * 
+ * ANYONE IN THE DEVELOPER COMMUNITY MAY USE THIS PROJECT FREELY
+ * FOR THEIR OWN DEVELOPMENT SELF-LEARNING OR DEVELOPMENT or LIVE PROJECT 
+ * 
+ * @author	Sabbir Hossain Rupom
+ * @since	Version 1.0.0
+ * @filesource
+ */
+
 header_remove('X-Powered-By'); 
 
 define('APP_NAME', 'RESTful API Template (Flight PHP)');
