d8a51e3b0210399800e645a44c2a33fdb5fcd4d6
sabbir-rupom <sabbir.rupom@gmail.com>
2019-03-08 12:27:09 +0600

    Project author information updated

--- public/index.php
+++ public/index.php
@@ -1,4 +1,17 @@
 <?php
+/*
+ * RESTful API Template
+ * 
+ * A RESTful API template based on flight-PHP framework
+ * This software project is based on my recent REST-API development experiences. 
+ * 
+ * ANYONE IN THE DEVELOPER COMMUNITY MAY USE THIS PROJECT FREELY
+ * FOR THEIR OWN DEVELOPMENT SELF-LEARNING OR DEVELOPMENT or LIVE PROJECT 
+ * 
+ * @author	Sabbir Hossain Rupom
+ * @since	Version 1.0.0
+ * @filesource
+ */
 
 header_remove('X-Powered-By'); 
 
