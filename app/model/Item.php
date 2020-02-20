8980b32d09d59487786f179169dd51e942921d07
Sabbir Hossain Rupom <web_one@pulseservicesbd.com>
2020-02-20 23:12:56 +0600

    Example models updated

---
+++ app/model/Item.php
@@ -0,0 +1,38 @@
+<?php
+
+(defined('APP_NAME')) or exit('Forbidden 403');
+
+namespace Model;
+
+/**
+ * Item model class.
+ */
+class Item extends BaseModel
+{
+
+    /* Table Definition */
+    const TABLE_NAME = "items";
+    const PRIMARY_KEY = 'item_id';
+
+    /**
+     * Column Defination
+     */
+    protected static $columnDefs = array(
+        'item_id' => array(
+            'type' => 'int',
+            'json' => true
+        ),
+        'item_name' => array(
+            'type' => 'string',
+            'json' => true
+        ),
+        'created_at' => array(
+            'type' => 'string',
+            'json' => false
+        ),
+        'updated_at' => array(
+            'type' => 'string',
+            'json' => false
+        )
+    );
+}
