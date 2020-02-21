fc4f16a29a94178060ac7114460b1879be77a324
Sabbir Hossain Rupom <web_one@pulseservicesbd.com>
2020-02-21 11:41:00 +0600

    Test API modified
    Ouput class bug is fixed
    Format class bug is fixed
    Route file is updated
    constant file bug is fixed
    Test API file corrected

--- app/api/Test.php
+++ app/api/Test.php
@@ -2,8 +2,10 @@
 
 (defined('APP_NAME')) or exit('Forbidden 403');
 
-use System\Config;
 use System\Core\Model\Cache as CacheModel;
+use Helper\DateUtil;
+use flight\net\Request;
+use System\Message\ResultCode;
 
 /**
  * Description of Test.
@@ -15,6 +17,10 @@ use System\Core\Model\Cache as CacheModel;
  */
 class Test extends BaseClass
 {
+    public function __construct(Request $request, $value, $apiName)
+    {
+        parent::__construct($request, $value, $apiName);
+    }
 
     /**
      * Processing API script execution.
@@ -22,14 +28,112 @@ class Test extends BaseClass
     public function action()
     {
 
-//        $cache = $this->config->cacheService();
-//
-//        $tmp_object = new stdClass;
-//        $tmp_object->str_attr = 'test';
-//        $tmp_object->int_attr = 123;
-//
-//        CacheModel::addCache($cache, 'okata', $tmp_object, 100);
-//        $get_result = CacheModel::getCache($cache, 'okata');
+        /**
+         *  Check database connection with PDO driver
+         */
+        if ($this->pdo instanceof PDO) {
+            $responseArray['DB'] = 'Database is properly connected';
+        } else {
+            $responseArray['DB'] = 'Database is not connected properly. Please check config_app.ini';
+        }
+
+        /**
+         * Check application logger is functional or not
+         */
+        $checkLoggerStatus = true;
+        if ($this->config->isLogEnable()) {
+            $logPath = $this->config->getAppLogPath();
+            if (!file_exists($logPath) && !is_dir($logPath)) {
+                if (!mkdir($logPath, 0755, true)) {
+                    $responseArray['Log'] = 'Log folder cannnot be created. Please change folder permission for apache access : ' . $logPath;
+                    $checkLoggerStatus = false;
+                }
+            } else {
+                if (!is_writable($logPath)) {
+                    $responseArray['Log'] = 'Log folder is not writable. Please change file permission for apache access : ' . $logPath;
+                    $checkLoggerStatus = false;
+                }
+            }
+
+            if ($checkLoggerStatus) {
+                $responseArray['Log'] = 'System application log is functional';
+            }
+        } else {
+            $responseArray['Log'] = 'System application log is disabled from config_app.ini';
+        }
+
+        /**
+         * Check cache system is functional or not
+         */
+        if ($this->config->isServerCacheEnable()) {
+            $message1 = 'Local filecache system is functional';
+            if ($this->config->isLocalFileCacheEnable()) {
+                /**
+                 * Check local cache path access permission
+                 */
+                $cachePath = $this->config->getLocalCachePath();
+                if (!file_exists($cachePath) && !is_dir($cachePath)) {
+                    if (!mkdir($cachePath, 0755, true)) {
+                        $message1 = 'Cache folder cannnot be created. Please change folder permission for apache access : ' . $cachePath;
+                    }
+                } else {
+                    if (!is_writable($cachePath)) {
+                        $message1 = 'Cache folder is not writable. Please change file permission for apache access : ' . $cachePath;
+                    }
+                }
+            } else {
+                $message1 = 'Local filecache system is disabled from config_app.ini';
+            }
+
+            // Memcache system test case
+            $key = 'akaash_test';
+            $msgValue = 'Memcache system is functional';
+
+            if (!extension_loaded('memcache')) {
+                $message2 = 'Memcache module is not installed';
+            } else {
+                $cache = new \System\Cache\Memcached($this->config->getMemcacheHost(), $this->config->getMemcachePort());
+
+                // clear all existing cache data
+                CacheModel::clearCache($cache);
+
+                // set sample cache data
+                CacheModel::addCache($cache, $key, $msgValue, 120);
+
+                $message2 = !empty(CacheModel::getCache($cache, $key)) ? $msgValue : 'Memcache system is not functional. Please check memcache settings.';
+            }
+            $responseArray['Cache'] = array(
+                'filecache' => $message1,
+                'memcache' => $message2,
+            );
+        } else {
+            $responseArray['Cache'] = 'Server cache feature is disabled from config';
+        }
+
+        // Check file upload path access permission
+
+        if (!file_exists(UPLOAD_PATH) && !is_dir(UPLOAD_PATH)) {
+            if (!mkdir(UPLOAD_PATH, 0644, true)) {
+                $responseArray['Upload'] = 'Upload folder cannnot be created. Please change folder permission for apache access : ' . UPLOAD_PATH;
+            }
+        } else {
+            if (!is_writable(UPLOAD_PATH)) {
+                $responseArray['Upload'] = 'Upload folder is not writable. Please change folder permission for apache access : ' . UPLOAD_PATH;
+            } else {
+                $responseArray['Upload'] = 'File upload directory permission is set properly';
+            }
+        }
+
+        if (!empty($this->value)) {
+            $responseArray['value'] = $this->value;
+        }
+
+        return [
+            'result_code' => ResultCode::SUCCESS,
+            'time' => DateUtil::getToday(),
+            'data' => $responseArray,
+            'error' => []
+        ];
 
 //        Model\LogAPI::startTransaction($this->pdo);
 //
@@ -53,9 +157,5 @@ class Test extends BaseClass
 //        if (!empty($result)) {
 //            $result = Model\LogAPI::findAllBy([Model\LogAPI::PRIMARY_KEY => $id], $this->pdo);
 //        }
-
-        return [
-          'data' => 'found'
-        ];
     }
 }
--- app/config/route.php
+++ app/config/route.php
@@ -7,33 +7,35 @@ use System\Message\ResultCode as ResultCode;
 if (!isset($argv)) {
     // Set root / index page response view
 
+    $apiUriPrefix = ''; // e.g /api
+
     Flight::route('/', function () {
         Flight::json(array(
-            'data' => array(
-                'title' => 'Welcome',
-                'message' => 'This REST-API template is built in PHP, with flight micro-framework as engine',
-            ),
-            'result_code' => ResultCode::SUCCESS,
+          'data' => array(
+            'title' => 'Welcome',
+            'message' => 'Akaash is a REST-API template is built in PHP, with flight micro-framework as engine',
+          ),
+          'result_code' => ResultCode::SUCCESS,
         ));
     });
 
     // Set route for GET|POST|PUT|PATCH|DELETE query request from client
-    Flight::route('GET|POST|PUT|PATCH|DELETE /api/@name', array('Core\Controller', 'initAPI'));
+    Flight::route('GET|POST|PUT|PATCH|DELETE ' . $apiUriPrefix . '/@name', array('Core\Controller', 'initAPI'));
 
     // Set group route for GET|POST|PUT|PATCH|DELETE query request from client
-    Flight::route('GET|POST|PUT|PATCH|DELETE /api/@group/@name', array('Core\Controller', 'initGroupAPI'));
+    Flight::route('GET|POST|PUT|PATCH|DELETE ' . $apiUriPrefix . '/@group/@name', array('Core\Controller', 'initGroupAPI'));
 
     // Set group route for GET|POST|PUT|PATCH|DELETE query request from client
-    Flight::route('GET|POST|PUT|PATCH|DELETE /api/@group/@name/@value', array('Core\Controller', 'initGroupAPIwithParam'));
+    Flight::route('GET|POST|PUT|PATCH|DELETE ' . $apiUriPrefix . '/@group/@name/@value', array('Core\Controller', 'initGroupAPIwithParam'));
 
     // Set error page page response
     Flight::map('notFound', function () {
         Flight::json(array(
-            'error' => array(
-                'title' => 'Data Not Found',
-                'message' => 'Requested data not found',
-            ),
-            'result_code' => ResultCode::NOT_FOUND,
+          'error' => array(
+            'title' => 'Data Not Found',
+            'message' => 'Requested data not found',
+          ),
+          'result_code' => ResultCode::NOT_FOUND,
         ));
     });
 }
